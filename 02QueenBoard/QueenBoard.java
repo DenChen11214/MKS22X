4fa3efdea3d9e5344a102ecdbbc9f23699af24b0
Dennis Chen <dchen22@stuy.edu>
2018-02-11 18:33:02 -0500

    Created the toString and tested my addQueen, which didnt work

--- 02QueenBoard/QueenBoard.java
+++ 02QueenBoard/QueenBoard.java
@@ -1,51 +1,71 @@
+import java.util.*;
 public class QueenBoard{
-    private int[][] board;
-    public QueenBoard(int size){
-	board = new int[size][size];
+  private int[][] board;
+  public QueenBoard(int size){
+    board = new int[size][size];
+  }
+  private boolean addQueen(int r, int c){
+    if(board[r][c] >= 1){
+	    return false;
     }
-    private boolean addQueen(int r, int c){
-	if(board[r][c] >= 1){
+    if(board[r][c] == -1){
 	    return false;
-	}
-	if(board[r][c] == -1){
-	    return false;
-	}
-	if(c  < board[0].length -1){
+    }
+    if(c  < board[0].length -1){
 	    for(int col = c + 1; col < board[0].length;col++){
-		board[r][col] += 1;
+        board[r][col] += 1;
 	    }
 	    if(r < board.length - 1 && r > 0){
-		int row = r - 1;
-		int col = c + 1;
-		while(row - 1 > 0 && col + 1 < board[0].length){
-		    board[row][col] += 1;
-		    row--;
-		    col++;
-		}
-		int ro = r + 1;
-		int co = c + 1;
-		while(ro + 1 < board.length && co + 1 < board[0].length){
-		    board[ro][co] +=1;
-		    ro++;
-		    col++;
-		}
+        int row = r - 1;
+        int col = c + 1;
+        while(row - 1 > 0 && col + 1 < board[0].length){
+          board[row][col] += 1;
+          row--;
+          col++;
+        }
+        int ro = r + 1;
+        int co = c + 1;
+        while(ro + 1 < board.length && co + 1 < board[0].length){
+          board[ro][co] +=1;
+          ro++;
+          col++;
+        }
 	    }
-	}
-	return true;
     }
+    return true;
+  }
 	
-    private boolean removeQueen(int r, int c){
-	return true;
+  private boolean removeQueen(int r, int c){
+    return true;
+  }
+  public String toString(){
+    String output =  "";
+    for(int r = 0;r < board.length;r++){
+      for(int c = 0; c< board[0].length;c++){
+        if(board[r][c] == -1){
+          output += "Q ";
+        }
+        else{
+          output += board[r][c] + " ";
+        }
+      }
+      output += "\n";
     }
-    public String toString(){
-	return "";
-    }
-    //use helper
-    public boolean solve(){
-	return true;
-    }
-    //use helper
-    public int countSolutions(){
-	return 0;
-    }
+    return output;
+  }
+  //use helper
+  public boolean solve(){
+    return true;
+  }
+  //use helper
+  public int countSolutions(){
+    return 0;
+  }
+  public static void main(String[] args){
+    QueenBoard q = new QueenBoard(4);
+    System.out.println(q);
+    q.addQueen(1,1);
+    System.out.println(q);
+    System.out.println(Arrays.deepToString(q.board));
+  }
 }
