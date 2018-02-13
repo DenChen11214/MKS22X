d96014a9223f615ae36207c77d795c672fe28609
DenChen11214 <dchen22@stuy.edu>
2018-02-13 11:35:05 -0500

    wrote exceptions

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,6 +1,8 @@
 public class KnightBoard{
     private int[][] board;
     public KnightBoard(int startingRows, int startingCols){
+	if(startingRows < 0 || startingCols < 0){
+	    throw new IllegalArgumentException();
 	board = new int[startingRows][startingCols];
     }
     public String toString(){
@@ -18,7 +20,31 @@ public class KnightBoard{
 	}
 	return output;
     }
+    private void nonZero(){
+	for(int r = 0;r < board.length; r ++){
+	    for(int c = 0; c < board[0].length;c++){
+		if(board[r][c] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+    }
+    private boolean
     public boolean solve(int startingRow, int startingCol){
+	if(startingRows < 0 || startingCols < 0){
+	    throw new IllegalArgumentException();
+	}
+	nonZero();
+	return solveHelp(int startingRow, int startingCol, 0);
+    }
+    private boolean solveHelp(int row, int col, int level){
+	int[][] moves = new int[8][8];
+	
+	if(startingRows < 0 || startingCols < 0){
+	    throw new IllegalArgumentException();
+	}
+	nonZero();
+	
 	return true;
     }
     public int countSolutions(int startingRow, int startingCol){
@@ -26,7 +52,6 @@ public class KnightBoard{
     }
     public static void main(String[] args){
 	KnightBoard k = new KnightBoard(5,5);
-	k.board[0][1] = 20;
 	System.out.println(k);
     }
 }
