8cb245d63313382bf535c068af6624b0e0aa9ceb
DenChen11214 <dchen22@stuy.edu>
2018-02-27 11:34:59 -0500

    worked on constructor, made it read the maze and check if its valid, put maze into a 2d array

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -1,40 +1,89 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 public class Maze{
-  private String[][] mazeBoard;
-  public Maze(){
-    mazeBoard = new String[9][35];
-  }
-  public void readMaze(String maze){
-    try{
-      int r = 0;
-      File f = new File(maze);
-      Scanner inf = new Scanner(f);
-      while(inf.hasNextLine()){
-        String line = inf.nextLine();
-        for(int c = 0; c < 35; c ++){
-          mazeBoard[r][c] = line.substring(c, c + 1);
-        }
-        r +=1;
-      }
+    private char[][] mazeBoard;
+    private boolean animate;
+    public Maze(String filename) throws FileNotFoundException{
+	try{
+	    int s = 0;
+	    int e = 0;
+	    int r = 0;
+	    int c = 0;
+	    File f = new File(filename);
+	    Scanner inf = new Scanner(f);
+	    while(inf.hasNextLine()){
+		String line = inf.nextLine();
+		c = line.length();
+		for(int i = 0; i < line.length(); i++){
+		    if(line.charAt(i) == 'S'){
+			s++;
+		    }
+		    if(line.charAt(i) == 'E'){
+			e++;
+		    }
+		}
+		r++;
+	    }
+	    if(s != 1 || e != 1){
+		throw new IllegalStateException();
+	    }
+	    mazeBoard = new char[r][c];
+	    this.readMaze(filename);
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("No such file exists");
+	}
     }
-    catch(FileNotFoundException e){
+    private void wait(int millis){
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
     }
-  }
-  public String toString(){
-    String str = "";
-    for(int r = 0; r < mazeBoard.length;r++){
-      for(int c = 0;c < mazeBoard[0].length;c++){
-        str += mazeBoard[r][c];
-      }
-      str += "\n";
+    public void setAnimate(boolean b){
+        animate = b;
     }
-    return str;
-  }
-  public static void main(String args[]){
-    Maze m = new Maze();
-    m.readMaze("Maze1.txt");
-    System.out.println(m);
-  }
+    public void clearTerminal(){
+        //erase terminal, go to top left of screen.
+        System.out.println("\033[2J\033[1;1H");
+    }
+    private int solve(int row, int col){
+	if(animate){
+	    clearTerminal();
+	    System.out.println(this);
+	    wait(20);
+	}	 
+	return -1; 
+    }
+    public void readMaze(String maze){
+	try{
+	    int r = 0;
+	    File f = new File(maze);
+	    Scanner inf = new Scanner(f);
+	    while(inf.hasNextLine()){
+		String line = inf.nextLine();
+		for(int c = 0; c < line.length(); c ++){
+		    mazeBoard[r][c] = line.charAt(c);
+		}
+		r +=1;
+	    }
+	}
+	catch(FileNotFoundException e){
+	}
+    }
+    
+    public String toString(){
+	String str = "";
+	for(int r = 0; r < mazeBoard.length;r++){
+	    for(int c = 0;c < mazeBoard[0].length;c++){
+		str += mazeBoard[r][c];
+	    }
+	    str += "\n";
+	}
+	return str;
+    }
+    public static void main(String args[]){
+	Maze m = new Maze("Maze1.txt");
+    }
 }
