d67f8f84663bb1312403dd45f8bb7e1ade5da4f1
DenChen11214 <dchen22@stuy.edu>
2018-03-06 11:34:35 -0500

    started on bronze

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -2,6 +2,22 @@ import java.util.*;
 import java.io.*;
 public class USACO{
     public static int bronze(String filename){
+	try{
+	    int r = 0;
+	    int c = 0;
+	    File f = new File(filename);
+	    Scanner in = new Scanner(f);
+	    while(in.hasNextLine()){
+		String line = in.nextLine();
+		if(r == 0){
+		    for(int i = 0; i < line.length() ; i++){
+			while(line.charAt(i) != ' '){
+			}
+		    }
+		}
+	    }
+	}
+			    
 	return 0;
     }
     public static int silver(String filename){
