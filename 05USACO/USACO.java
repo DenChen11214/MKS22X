111ea0e23456a4e4b43cd5f90b80d7e12c5bbbd3
Dennis Chen <dchen22@stuy.edu>
2018-03-08 19:47:55 -0500

    finished silver, only tested on 1st test case, not recursive

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -82,24 +82,67 @@ public class USACO{
 	    int t = in.nextInt();
 	    char[][] land = new char[r][c];
 	    int ro = 0;
+      int r1 = 0;
+      int c1 = 0;
+      int r2 = 0;
+      int c2 = 0;
 	    while(in.hasNextLine()){
         String line = in.nextLine();
-        System.out.println(line);
-        if(ro < r && ro != 0){
+        if(ro <= r && ro != 0){
           for(int col = 0; col < c;col++){
-            land[ro][col] = line.charAt(col);
-            System.out.println(land[ro][col]);
+            land[ro - 1][col] = line.charAt(col);
           }
         }
         if(ro == r + 1){
-          int r1 = Character.getNumericValue(line.charAt(0));
-          int c1 = Character.getNumericValue(line.charAt(2));
-          int r2 = Character.getNumericValue(line.charAt(4));
-          int c2 = Character.getNumericValue(line.charAt(6));
+          r1 = Character.getNumericValue(line.charAt(0));
+          c1 = Character.getNumericValue(line.charAt(2));
+          r2 = Character.getNumericValue(line.charAt(4));
+          c2 = Character.getNumericValue(line.charAt(6));
         }
         ro++;
       }
-
+      int[][] curPath = new int[r][c];
+      int[][] newPath = new int[r][c];
+      int[][] template = new int[r][c];
+      curPath[r1 - 1][c1 - 1] = 1;
+      for(int row = 0;row < r - 1;row++){
+        for(int col = 0;col < c - 1;col++){
+          if(land[row][col] == '*'){
+            curPath[row][col] = -1;
+            newPath[row][col] = -1;
+            template[row][col] = -1;
+          }
+        }
+      }
+      int[] rowM = {-1,0,1,0};
+      int[] colM = {0,-1,0,1};
+      for(int i = 0; i < t;i++){
+        for(int row = 0;row < r ;row++){
+          for(int col = 0;col < c ;col++){
+            if(curPath[row][col] != -1){
+              if(curPath[row][col] > 0){
+                newPath[row][col] = 0;
+              }
+              for(int n = 0; n < 4;n++){
+                if((row  + rowM[n] < land.length) &&
+                   col + colM[n]< land[0].length &&
+                   row  + rowM[n] >= 0 &&
+                   col + colM[n] >= 0 &&
+                   curPath[row + rowM[n]][col + colM[n]] != -1){
+                  newPath[row][col] += curPath[row + rowM[n]][col + colM[n]];
+                }
+              }
+            }
+          }
+        }
+        for(int row = 0;row < r;row++){
+          for(int col = 0;col < c;col++){
+            curPath[row][col] = newPath[row][col];
+            newPath[row][col] = template[row][col];
+          }
+        }
+      }
+      return curPath[r2 - 1][c2 - 1];
     }
     catch(FileNotFoundException e){
 	    System.out.println("No such file exists");
