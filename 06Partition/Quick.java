4ff3aa093ad6cc01e31d0f6caddc72c0f241326a
DenChen11214 <dchen22@stuy.edu>
2018-03-15 11:34:36 -0400

    new partition works sometimes

--- 06Partition/Quick.java
+++ 06Partition/Quick.java
@@ -1,65 +1,74 @@
 import java.util.*;
 public class Quick{
-  public static int part(int[] data,int start, int end){
-    Random a = new Random();
-    int in = 0;
-    if(end - start == 0){
-      in = start;
+    public static int part(int[] data,int start, int end){
+	Random a = new Random();
+	int in = 0;
+	if(end - start == 0){
+	    in = start;
+	}
+	else{
+	    in = a.nextInt(end - start) + start;
+	}
+	int i1 = data[in];
+	int small = start + 1;
+	int large = end;
+	int index = small;
+	int temp  = data[start];
+	data[start] = i1;
+	data[in] = temp;
+	while(index <= large){
+	    if(data[index] == i1){
+		index++;
+	    }
+	    if(data[index] > i1){
+		temp = data[index];
+		data[index] = data[large];
+		data[large] = temp;
+		large--;
+	    }
+	    else{
+		temp = data[index];
+		data[index] = data[small];
+		data[small] = temp;
+		small++;
+		index++;
+	    }
+	}
+	temp = data[small - 1];
+	data[small - 1] = i1;
+	data[start] = temp;
+	System.out.println(i1);
+	return large;
     }
-    else{
-      in = a.nextInt(end - start) + start;
+    public static int quickSelect(int[] data, int k){
+	int v1 = part(data,0,data.length - 1);
+	int s = 0;
+	int e = data.length - 1;
+	while(v1 + 1 != k){
+	    if(v1 + 1 > k){
+		e = v1 - 1;
+		v1 = part(data,s,e);
+	    }
+	    if(v1 + 1 < k){
+		s = v1 + 1;
+		v1 = part(data,s,e);
+	    }
+	}
+	return data[v1];
     }
-    int i1 = data[in];
-    int small = start + 1;
-    int large = end;
-    int temp  = data[start];
-    data[start] = i1;
-    data[in] = temp;
-    while(small <= large){
-      if(data[small] < i1){
-        small++;
-      }
-      else{
-        temp = data[small];
-        data[small] = data[large];
-        data[large] = temp;
-        large--;
-      }
+    public static void quickSort(int[] ary){
+	quickSortHelp(ary, 0, ary.length - 1);
     }
-    temp = data[large];
-    data[large] = i1;
-    data[start] = temp;
-    return large;
-  }
-  public static int quickSelect(int[] data, int k){
-    int v1 = part(data,0,data.length - 1);
-    int s = 0;
-    int e = data.length - 1;
-    while(v1 + 1 != k){
-      if(v1 + 1 > k){
-        e = v1 - 1;
-        v1 = part(data,s,e);
-      }
-      if(v1 + 1 < k){
-        s = v1 + 1;
-        v1 = part(data,s,e);
-      }
+    public static void quickSortHelp(int[] ary, int start, int end){
+	if(start != end + 1){
+	    int v1 =  part(ary, start, end);
+	    quickSortHelp(ary,start,v1 - 1);
+	    quickSortHelp(ary,v1 + 1, end);
+	}
     }
-    return data[v1];
-  }
-  public static void quickSort(int[] ary){
-    quickSortHelp(ary, 0, ary.length - 1);
-  }
-  public static void quickSortHelp(int[] ary, int start, int end){
-    if(start != end + 1){
-      int v1 =  part(ary, start, end);
-      quickSortHelp(ary,start,v1 - 1);
-      quickSortHelp(ary,v1 + 1, end);
+    public static void main(String[] args){
+	int[] dat = {1,0,0,2,0,1,2,0,1,2,0,0,0,1,2};
+	System.out.println(part(dat,0,14));
+	System.out.println(Arrays.toString(dat));
     }
-  }
-  public static void main(String[] args){
-    int[] dat = {2,1,4,61,75,4234,3753,32,11,1,1,1,1};
-    quickSort(dat);
-    System.out.println(Arrays.toString(dat));
-  }
 }
