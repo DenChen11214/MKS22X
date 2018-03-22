42c5833441d5aaf07e1b14f50a96dd93deb53c93
DenChen11214 <dchen22@stuy.edu>
2018-03-22 11:17:27 -0400

    tested merge using testing code and changed a little thing to make it a bit faster

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class Merge{
     public static void mergesort(int[] data){
-	int[] temp = new int [data.length];
+	int[] temp = new int[data.length];
+	for(int i = 0; i < data.length ; i ++){
+	    temp[i] = data[i];
+	}
 	msort(data,temp,0,data.length - 1);
     }
     private static void msort(int[] data, int[] temp, int low, int high){
-	for(int i  = low;i <= high;i++){
-	    temp[i] = data[i];
-	}
 	if(low >= high){
 	    return;
 	}
@@ -48,39 +48,9 @@ public class Merge{
 	}
     }
     public static void main(String[] args){
-	int arr[] = {12, 15, 23, 4 , 6, 10, 35, 28}; //Even number of element
-	mergesort(arr);
-	System.out.println(Arrays.toString(arr));
-	int arr1[]={}; //Empty list
-	mergesort(arr1);
-	System.out.println(Arrays.toString(arr1));
-	int arr2[] = {4, 6, 10, 12, 15, 23, 28, 35}; //already sorted array
-	mergesort(arr2);
-	System.out.println(Arrays.toString(arr2));
-	int arr3[] = {12, 15, 23, 4 , 6, 10, 35}; //odd length array
-	mergesort(arr3);
-	System.out.println(Arrays.toString(arr3));
-	int arr4[] = {35, 28, 23, 15, 12, 10, 6, 4}; //descending sorted array input
-	mergesort(arr4);
-	System.out.println(Arrays.toString(arr4));
-	int arr5[] = {12};//one element
-	mergesort(arr5);
-	System.out.println(Arrays.toString(arr5));
-	int arr6[] = {12, 4}; // two elements
-	mergesort(arr6);
-	System.out.println(Arrays.toString(arr6));
-	int arr7[] = {12, 15, 23, 4 , 6, 10, 35, 28, 100, 130, 500, 1000, 235, 554, 75, 345, 800, 222, 333, 888, 444, 111, 666, 777, 60};//large list of elements
-	mergesort(arr7);
-	System.out.println(Arrays.toString(arr7));
-	int arr8[] = {12, 15, -23, -4 , 6, 10, -35, 28}; //negative elements
-	mergesort(arr8);
-	System.out.println(Arrays.toString(arr8));
-	int arr9[] = {12, 12, 23, 4 , 6, 6, 10, -35, 28}; //duplicate elements
-	mergesort(arr9);
-	System.out.println(Arrays.toString(arr9));
-	int arr10[] = {12, 12, 12, 12, 12}; //Same element
-	mergesort(arr10);
-	System.out.println(Arrays.toString(arr10));
+        
+	int arr11[] = new int[10000000];
+	mergesort(arr11);
 	
     }
 }
