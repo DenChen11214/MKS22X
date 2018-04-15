a41e5ec224b4b2274f2ae67c769f7b7c8d3cc31d
Dennis Chen <dchen22@stuy.edu>
2018-04-15 19:50:46 -0400

    made radixsort work for neg

--- 08LinkedList/Sort.java
+++ 08LinkedList/Sort.java
@@ -7,7 +7,7 @@ public class Sort{
     int max = data.get(0);
     for(int i = 1; i < data.size();i++){
       int cur = data.get(i);
-      if(max < cur){
+      if(Math.abs(max) < Math.abs(cur)){
         max = cur;
       }
     }
@@ -16,7 +16,12 @@ public class Sort{
       int len = data.size();
       for(int i = 0; i < len;i++){
         int num = data.get(i);
-        if((num + "").length() >= k){
+        if(num < 0 && (num + "").substring(1,(num+ "").length()).length() >= k){
+          char last = (num + "").charAt((num + "").length() - k);
+          int n = last - '0';
+          data2[n].add(num);
+        }
+        else if(num > 0 && (num + "").length() >= k){
           char last = (num + "").charAt((num + "").length() - k);
           int n = last - '0';
           data2[n].add(num);
@@ -25,24 +30,45 @@ public class Sort{
           data2[0].add(num);
         }
       }
-      MyLinkedListImproved<Integer> temp = new MyLinkedListImproved<>();
-      for(int i = 0; i < 10; i++){
-        temp.extend(data2[i]);
+      for(int i = 0; i < data2.length;i++){
+        System.out.println(data2[i]);
       }
-      data2[0].clear();
-      data = temp;
+      int index = 0;
+      for(int i = 9; i >=0; i--){
+        if(data2[i].size() > 0){
+          index = i;
+        }
+      }
+      data.clear();
+      for(int i = 0; i < 10; i++){
+        data.extend(data2[i]);
+      }
+      data2[index].clear();
     }
+    for(Integer n : data){
+      if(n < 0){
+        data.remove(n);
+        data.add(0,n);
+      }
+    }
+    System.out.println(data);
   }
   public static void main(String[] args){
     MyLinkedListImproved<Integer> l = new MyLinkedListImproved<>();
-    l.add(0);
-    l.add(100);
-    l.add(132);
-    l.add(5);
-    l.add(44123);
-    l.add(82);
-    l.add(23);
+    l.add(-1);
+    l.add(-2311);
+    l.add(-132);
+    l.add(-11);
+    l.add(-2);
+    l.add(-10);
+    l.add(1);
+    l.add(1231);
+    l.add(12);
+    l.add(11);
+    l.add(6);
     l.add(3);
-    radixsort(l);
+    l.add(132);
+    Sort.radixsort(l);
+    System.out.println(l);
   }
 }
