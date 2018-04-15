eab8d6cf764e90e85de13d1168d1bc13c87fe2bc
Dennis Chen <dchen22@stuy.edu>
2018-04-15 18:32:13 -0400

    finished radixsort for pos ints and tested

--- 08LinkedList/Sort.java
+++ 08LinkedList/Sort.java
@@ -12,35 +12,34 @@ public class Sort{
       }
     }
     max = (max + "").length();
-    System.out.println(max);
     for(int k = 1; k <= max;k++){
-      for(int i = 0; i < data.size();i++){
+      int len = data.size();
+      for(int i = 0; i < len;i++){
         int num = data.get(i);
         if((num + "").length() >= k){
           char last = (num + "").charAt((num + "").length() - k);
           int n = last - '0';
           data2[n].add(num);
         }
+        else{
+          data2[0].add(num);
+        }
       }
-      for(int i =0;i < data2.length;i++){
-        System.out.println(data2[i]);
-      }
-      MyLinkedListImproved<Integer> temp = data2[0];
-      data2[0].clear();
-      for(int i = 1; i < 10; i++){
+      MyLinkedListImproved<Integer> temp = new MyLinkedListImproved<>();
+      for(int i = 0; i < 10; i++){
         temp.extend(data2[i]);
-        data2[i].clear();
       }
+      data2[0].clear();
       data = temp;
     }
   }
   public static void main(String[] args){
     MyLinkedListImproved<Integer> l = new MyLinkedListImproved<>();
     l.add(0);
-    l.add(0);
-    l.add(1);
+    l.add(100);
+    l.add(132);
     l.add(5);
-    l.add(4);
+    l.add(44123);
     l.add(82);
     l.add(23);
     l.add(3);
