d80732b952f629d8fdfe57c0bddb7494708332de
Dennis Chen <dchen22@stuy.edu>
2018-04-29 15:55:48 -0400

    created add and tested

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,26 +1,29 @@
-public class MyHeap<String extends Comparable<String>>{
+public class MyHeap{//<String extends Comparable<String>>{
   private int length;
   private boolean isMax;
-  private String[] data = new String[0];
+  private String[] data;
+  //@SuppressWarnings("unchecked")
   public MyHeap(){
     isMax = true;
     length = 0;
+    data =  new String[10];
+    //data = (String[])new Object[10];
   }
+  // @SuppressWarnings("unchecked")
   public MyHeap(boolean maxmin){
     isMax = maxmin;
     length = 0;
+    data = new String[10];
+    //data = (String[])new Object[10];
   }
   public int size(){
     return length;
-  }
-  public void add(String s){
-    resize();
   }
   public String getParent(int i){
     return data[(i - 1)/2];
   }
   public boolean hasParent(int i){
-    return ((i - 1) / 2) > 0;
+    return ((i - 1) / 2.0) >= 0;
   }
   public String getChildL(int i){
     return data[i * 2 + 1];
@@ -31,20 +34,71 @@ public class MyHeap<String extends Comparable<String>>{
   public boolean hasChildL(int i){
     return (i * 2 + 1) < size();
   }
-  public boolean hasChildL(int i){
+  public boolean hasChildR(int i){
     return (i * 2 + 2) < size();
   }
   public String peek(){
-    return data.get(0);
+    return data[0];
+  }
+  public void pushup(int i){
+    if(compares(isMax,data[i],getParent(i))){
+      swap(data, i,(i - 1) / 2);
+    }
+  }
+  public void add(String s){
+    resize();
+    int index = size();
+    data[index] = s;
+    length++;
+    while(hasParent(index)){
+      while(compares(isMax,data[index],getParent(index))){
+        pushup(index);
+        index = (index - 1) / 2;
+      }
+    }
+  }
+  private void swap(String[] ary,int i, int n){
+    String temp = ary[i];
+    ary[i] = ary[n];
+    ary[n] = temp;
+  }
+  public boolean compares(boolean maxmin, String child, String parent){
+    if(maxmin){
+      return child.compareTo(parent) >= 1;
+    }
+    else{
+      return child.compareTo(parent) < 1;
+    }
   }
   private void resize(){
     if(data.length - 1 == size()){
-      String[] newArray = new String[size * 2   ];
+      String[] newArray = new String[length * 2];
       for(int i = 0;i < size(); i++){
         newArray[i] = data[i];
       }
       data = newArray;
     }
   }
-
+  public String toString(){
+    String s = "[";
+    for(int i = 0;i < size() - 1; i++){
+      s += data[i] + ",";
+    }
+    return s + data[size() - 1] + "]";
+  }
+  public static void main(String[] args){
+    MyHeap heap = new MyHeap();
+    heap.add("a");
+    System.out.println(heap);
+    heap.add("b");
+    System.out.println(heap);
+    heap.add("c");
+    System.out.println(heap);
+    heap.add("d");
+    System.out.println(heap);
+    heap.add("e");
+    System.out.println(heap);
+    heap.add("f");
+    System.out.println(heap);
+  }
 }
